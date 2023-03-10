961477f87e7ac6129a41766f87ee58c942838e9a
Henning Noeth <hnoeth@apache.org>
2023-03-10 13:40:41 +0100

    adjust for arm64
    * add StandaloneFirefox build file

---
+++ buildStandaloneFirefox.sh
@@ -0,0 +1,27 @@
+NAMESPACE=selenium
+VERSION=4.10.0-20230607
+BASE=node-firefox
+BUILD_ARGS="--build-arg NAMESPACE=$NAMESPACE --build-arg VERSION=$VERSION --build-arg BASE=$BASE"
+
+echo "\033[1m> docker build ./Base ${BUILD_ARGS} -t $NAMESPACE/base:$VERSION -f ./Base/Dockerfile\033[0m"
+docker build ./Base ${BUILD_ARGS} -t $NAMESPACE/base:$VERSION -f ./Base/Dockerfile
+echo "\033[1m> docker build ./NodeBase ${BUILD_ARGS} -t $NAMESPACE/node-base:$VERSION -f ./NodeBase/Dockerfile\033[0m"
+docker build ./NodeBase ${BUILD_ARGS} -t $NAMESPACE/node-base:$VERSION -f ./NodeBase/Dockerfile
+echo "\033[1m> docker build ./NodeFirefox ${BUILD_ARGS} -t $NAMESPACE/node-firefox:$VERSION -f ./NodeFirefox/Dockerfile\033[0m"
+docker build ./NodeFirefox ${BUILD_ARGS} -t $NAMESPACE/node-firefox:$VERSION -f ./NodeFirefox/Dockerfile
+echo "\033[1m> docker build ./Standalone $BUILD_ARGS -t henningn/selenium-standalone-firefox:$VERSION-arm -f ./Standalone/Dockerfile\033[0m"
+docker build ./Standalone ${BUILD_ARGS} -t henningn/selenium-standalone-firefox:$VERSION-arm -f ./Standalone/Dockerfile
+echo "\033[1m> docker push henningn/selenium-standalone-firefox:$VERSION-arm\033[0m"
+docker push henningn/selenium-standalone-firefox:$VERSION-arm
+
+echo "\033[1m> docker pull $NAMESPACE/standalone-firefox:$VERSION\033[0m"
+docker pull $NAMESPACE/standalone-firefox:$VERSION
+echo "\033[1m> docker tag $NAMESPACE/standalone-firefox:$VERSION henningn/selenium-standalone-firefox:$VERSION-x86\033[0m"
+docker tag $NAMESPACE/standalone-firefox:$VERSION henningn/selenium-standalone-firefox:$VERSION-x86
+echo "\033[1m> docker push henningn/selenium-standalone-firefox:$VERSION-x86\033[0m"
+docker push henningn/selenium-standalone-firefox:$VERSION-x86
+
+echo "\033[1m> docker manifest create henningn/selenium-standalone-firefox:$VERSION henningn/selenium-standalone-firefox:$VERSION-arm henningn/selenium-standalone-firefox:$VERSION-x86\033[0m"
+docker manifest create henningn/selenium-standalone-firefox:$VERSION henningn/selenium-standalone-firefox:$VERSION-arm henningn/selenium-standalone-firefox:$VERSION-x86
+echo "\033[1m> docker manifest push henningn/selenium-standalone-firefox:$VERSION\033[0m"
+docker manifest push henningn/selenium-standalone-firefox:$VERSION
